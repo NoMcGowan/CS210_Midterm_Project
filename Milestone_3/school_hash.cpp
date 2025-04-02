1bb04407a5cd9c0058f66d939d1fa874c6175dfa
NoMcGowan <Tekkasunited@gmail.com>
2025-04-01 21:57:11 -0500

    Added hash table along with insert, delete, display, and search options

--- Milestone_3/school_hash.cpp
+++ Milestone_3/school_hash.cpp
@@ -16,3 +16,61 @@ struct School {
     School(string n, string a, string c, string s, string co)
         : name(n), address(a), city(c), state(s), county(co) {}
 };
+class SchoolHashTable {
+    private:
+        static const int TABLE_SIZE = 100;
+        vector<list<School>> table;
+    
+        int hashFunction(const string& key) {
+            int hash = 0;
+            for (char ch : key) {
+                hash += ch;
+            }
+            return hash % TABLE_SIZE;
+        }
+    
+    public:
+        SchoolHashTable() : table(TABLE_SIZE) {}
+    
+        void insert(const School& school) {
+            int index = hashFunction(school.name);
+            table[index].push_back(school);
+        }
+    
+        School* findByName(const string& name) {
+            int index = hashFunction(name);
+            for (auto& school : table[index]) {
+                if (school.name == name) {
+                    return &school;
+                }
+            }
+            return nullptr; 
+        }
+    
+        void deleteByName(const string& name) {
+            int index = hashFunction(name);
+            auto& chain = table[index];
+    
+            for (auto it = chain.begin(); it != chain.end(); ++it) {
+                if (it->name == name) {
+                    chain.erase(it);
+                    cout << "School deleted: " << name << endl;
+                    return;
+                }
+            }
+            cout << "School not found for deletion.\n";
+        }
+    
+        void display() {
+            for (int i = 0; i < TABLE_SIZE; ++i) {
+                if (!table[i].empty()) {
+                    cout << "Index " << i << ":\n";
+                    for (const auto& school : table[i]) {
+                        cout << "  " << school.name << ", " << school.address << ", " 
+                             << school.city << ", " << school.state << ", " << school.county << endl;
+                    }
+                }
+            }
+        }
+    };
+    
